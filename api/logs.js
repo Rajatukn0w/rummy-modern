ec6c38522b0d80ee14d63a7ca1ca288fb9c535ef
Rajatukn0w <dronzarlive900@gmail.com>
2025-06-23 13:03:38 +0530

    Update logs.js

--- api/logs.js
+++ api/logs.js
@@ -1,6 +1,19 @@
-import { clicks } from "./pixel.js";
+// /api/logs.js
 
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).json(clicks);
+export default async function handler(req, res) {
+  const SUPABASE_URL = "https://bbzhdylabnexgpcjmxfz.supabase.co";
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+  const table = "pixel_clicks";
+
+  const result = await fetch(`${SUPABASE_URL}/rest/v1/${table}`, {
+    method: "GET",
+    headers: {
+      apikey: SUPABASE_KEY,
+      Authorization: `Bearer ${SUPABASE_KEY}`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  const data = await result.json();
+  res.status(200).json(data);
 }
