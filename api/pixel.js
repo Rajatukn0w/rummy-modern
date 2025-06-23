cdd65389aee0afac167671c9054429db23435588
Rajatukn0w <dronzarlive900@gmail.com>
2025-06-23 13:18:03 +0530

    Update pixel.js

--- api/pixel.js
+++ api/pixel.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { id = "unknown" } = req.query;
 
   const SUPABASE_URL = "https://bbzhdylabnexgpcjmxfz.supabase.co";
-  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiemhkeWxhYm5leGdwY2pteGZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2NjI1ODgsImV4cCI6MjA2NjIzODU4OH0.UHncNkqmy7hYt9wCpzumT2gRdEcHP3bJcitA20LlqSY"
   const table = "pixel_clicks";
 
   const result = await fetch(`${SUPABASE_URL}/rest/v1/${table}?id=eq.${id}`, {
